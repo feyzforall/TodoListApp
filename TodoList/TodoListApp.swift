73252be8016beb5f9aafaab91da900226e200e7f
Feyzullah Kodat <feyzkodat@gmail.com>
2023-02-09 09:03:51 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Feyzullah Kodat on 9.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
